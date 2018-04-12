05350ba45f0cb822c93449c262ccc6719a4a7f25
Ortich <d.ortich.oficial@gmail.com>
2018-04-12 13:07:14 +0200

    Main activity terminado TODO: -Texto de los botones -Tags de los botones -Personalizar un poco -Añadir los gifs

--- app/src/main/java/com/example/usuario/isonidos/MainActivity.java
+++ app/src/main/java/com/example/usuario/isonidos/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.usuario.isonidos;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
+
+    SoundPool sonidos;
+    Field[] nombreCanciones = R.raw.class.getFields();
+    int[] arraySonidos = new int[nombreCanciones.length];
+    int idCancion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sonidos = new SoundPool(4, AudioManager.STREAM_MUSIC,0);
+    }
+
+    @Override
+    protected  void onStart() {
+        super.onStart();
+        for(int i = 0; i<arraySonidos.length; i++){
+            try {
+                idCancion = nombreCanciones[i].getInt(nombreCanciones[i]);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            arraySonidos[i] = sonidos.load(this, idCancion , 1);
+        }
+    }
+
+    public void sonido(View vista){
+        Button b = (Button) findViewById(vista.getId());
+        int numSonido = Integer.valueOf(b.getTag().toString());
+        sonidos.play(arraySonidos[numSonido] ,1,1,1,0,1);
     }
 }
